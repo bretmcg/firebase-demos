eaaf6c1a0e38333d7f28e6e7c466342c8e90119b
Bret McGowen <bretmcg@gmail.com>
2017-10-30 21:06:05 -0400

    Some changes, dunno

--- firebase-cloud-messaging/public/firebase-messaging-sw.js
+++ firebase-cloud-messaging/public/firebase-messaging-sw.js
@@ -1,9 +1,17 @@
 importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
 
+// TODO: paste Firebase something somethign
+// 
 // Initialize Firebase.
-// TODO: paste config from Firebase console at console.firebase.google.com
-// var config = { ... } ... firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyB56Ja_HTm0o3_kqIN8zkHIVQ5cH_m0Csk",
+  authDomain: "bret-google.firebaseapp.com",
+  databaseURL: "https://bret-google.firebaseio.com",
+  storageBucket: "project--200264200989872459.appspot.com",
+  messagingSenderId: "244962545401"
+};
+firebase.initializeApp(config);
 
 const messaging = firebase.messaging();
 
--- firebase-cloud-messaging/public/messaging.js
+++ firebase-cloud-messaging/public/messaging.js
@@ -1,21 +1,31 @@
 // Initialize Firebase.
 // TODO: paste config from Firebase console at console.firebase.google.com
-// var config = { ... } ... firebase.initializeApp(config);
+var config = {
+    apiKey: "AIzaSyB56Ja_HTm0o3_kqIN8zkHIVQ5cH_m0Csk",
+    authDomain: "bret-google.firebaseapp.com",
+    databaseURL: "https://bret-google.firebaseio.com",
+    storageBucket: "project--200264200989872459.appspot.com",
+    messagingSenderId: "244962545401"
+  };
+firebase.initializeApp(config);
+
+console.log('omg this SUPER SO embarassing');
 
 // Enable Firebase Cloud Messaging.
-// const messaging = firebase.messaging();
-// messaging.requestPermission()
-//   .then(function() {
-//     console.log('Have permission');
-//     return messaging.getToken();
-//   })
-//   .then(function(token) {
-//     console.log(token);
-//   })
-//   .catch(function(err) {
-//     console.log(err);
-//   });
+const messaging = firebase.messaging();
+messaging.requestPermission()
+  .then(function() {
+    console.log('Have permission');
+    return messaging.getToken();
+  })
+  .then(function(token) {
+    console.log(token);
+    // Normally, this is where you would upload to some server side code...
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 
-// messaging.onMessage(function(payload) {
-//   console.log('onMessage', payload);
-// });
+messaging.onMessage(function(payload) {
+  console.log('onMessage', payload);
+});
